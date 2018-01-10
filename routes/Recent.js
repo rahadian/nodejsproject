6c0d4b22812f2a9c7848411f8db654ed9cd02b82
adim_sikiji <felis@canisnfelis.com>
2018-01-10 14:23:06 +0700

    Update Recent

--- routes/Recent.js
+++ routes/Recent.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var Task = require('../models/Recent');
 
-router.get('/:year?/:month?/:day?/:publish?/:fokus?/:kolom?/',function(req,res,next){   
-    if(req.params.year,req.params.day-1,req.params.month,req.params.publish,req.params.fokus,req.params.kolom){
+router.get('/:publish?/:fokus?/:kolom?/',function(req,res,next){   
+    if(req.params.publish,req.params.fokus,req.params.kolom){
         var dateFormat = new Date(Date.UTC(req.params.year,req.params.day-1,req.params.month,0,0));
-        Task.getTaskById(dateFormat,req.params.publish,req.params.fokus,req.params.kolom,function(err,rows){
+        Task.getTaskById(req.params.publish,req.params.fokus,req.params.kolom,function(err,rows){
             
             if(err)
             {
